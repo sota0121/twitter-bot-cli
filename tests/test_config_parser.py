4e03cae5a7ebe84e4be8da2c790231480d4a883a
Sota Masuda <sota.masuda@the-lionsdata.com>
2022-02-04 00:27:52 +0900

    Sucessfully Added unit test with tempfile

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -2,21 +2,39 @@ import os
 import tempfile
 
 import pytest
+import yaml
 
 from tbc.config_parser import CfgParser
 
 
-class TestCfgParser:
-    def setup(self):
-        pass
 
-    def test_load_envval(self):
-        test_file_name = './tests/test.yml'
-        cfg = "ENV_1: 123\nENV_2: abc"
-        with open(test_file_name, 'w') as f:
-            f.write(cfg)
+# file fixture
+# https://docs.pytest.org/en/6.2.x/tmpdir.html
+# https://docs.pytest.org/en/6.2.x/tmpdir.html#the-tmpdir-factory-fixture
+# https://rinatz.github.io/python-book/ch08-02-pytest/
+# -------------------------------
+# Arrange-Act-Assert Style
+# http://wiki.c2.com/?ArrangeActAssert
 
-        CfgParser(test_file_name)
-        os.unlink(test_file_name)
-        assert os.getenv('ENV_1') == '123'
-        assert os.getenv('ENV_2') == 'abc'
+
+cfg_obj = {"ENV_1": '123', "ENV_2": "abc"}
+
+# Arange
+@pytest.fixture
+def config_file(tmpdir):
+    """config file"""
+    tmpfile = tmpdir.join("config.yml")
+    with tmpfile.open('w') as f:
+        cfg_str = yaml.dump(cfg_obj)
+        f.write(cfg_str)
+    yield str(tmpfile)
+    tmpfile.remove()
+
+
+def test_load_envval(config_file):
+    # Act - set value to envval
+    CfgParser(config_file)
+
+    # Assert
+    assert os.getenv('ENV_1') == '123'
+    assert os.getenv('ENV_2') == 'abc'
